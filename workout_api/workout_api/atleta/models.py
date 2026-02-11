3ac82dcc1987e6f5af3dfc23a72a482876643a78
eric-moniz <ericmoniz@outlook.com.br>
2026-02-10 22:32:01 -0300

    Criacao de schemas e models - entidades Categoria e Centro de treinamento

--- workout_api/workout_api/atleta/models.py
+++ workout_api/workout_api/atleta/models.py
@@ -1,9 +1,9 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime, Float, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime, Float, ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from workout_api.contrib.models import BaseModel
+from workout_api.contrib.models import BaseModel, CategoriaModel, CentroTreinamentoModel
 
 
 class AtletaModel(BaseModel):
@@ -17,3 +17,7 @@ class AtletaModel(BaseModel):
     altura: Mapped[float] = mapped_column(Float, nullable=False)
     sexo: Mapped[str] = mapped_column(String(1), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    categoria: Mapped["CategoriaModel"] = relationship(back_populates="atleta")
+    categoria_id = Mapped[int] = mapped_column(ForeignKey("categorias.pk_id"))
+    centro_treinamento: Mapped["CentroTreinamentoModel"] = relationship(back_populates="atleta")
+    centro_treinamento_id = Mapped[int] = mapped_column(ForeignKey("centros_treinamento.pk_id"))
---
+++ workout_api/workout_api/centro_treinamento/schemas.py
@@ -0,0 +1,18 @@
+from typing import Annotated
+
+from pydantic import Field
+
+from workout_api.contrib.schemas import BaseSchema
+
+
+class CentroTreinamento(BaseSchema):
+    nome: Annotated[
+        str, Field(description="Nome do centro de treinamento", examples="CT King", max_length=20)
+    ]
+    endereco: Annotated[
+        str, Field(description="Endereço do centro de treinamento", examples="Rua X, 102", max_length=60)
+    ]
+    proprietario: Annotated[
+        str,
+        Field(description="Nome do proprietário docentro de treinamento", examples="Marcos", max_length=30),
+    ]
