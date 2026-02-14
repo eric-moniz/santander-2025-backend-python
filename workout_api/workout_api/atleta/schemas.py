1859c64c0a68918c36b2b2650a42970e8f33672f
eric-moniz <ericmoniz@outlook.com.br>
2026-02-14 15:57:24 -0300

    Inserindo configuracoes do banco de dados e adicionando settings

--- workout_api/workout_api/atleta/schemas.py
+++ workout_api/workout_api/atleta/schemas.py
@@ -2,7 +2,7 @@ from typing import Annotated
 
 from pydantic import Field, PositiveFloat
 
-from workout_api.contrib.schemas import BaseSchema
+from workout_api.contrib.schemas import BaseSchema, OutMixin
 
 
 class Atleta(BaseSchema):
@@ -12,3 +12,11 @@ class Atleta(BaseSchema):
     peso: Annotated[PositiveFloat, Field(description="Peso do atleta em kilos", examples=65.8)]
     altura: Annotated[PositiveFloat, Field(description="Altura do atleta em metros", examples=1.73)]
     sexo: Annotated[str, Field(description="Sexo do atleta", examples="M", max_length=1)]
+
+
+class AtletaIn(Atleta):
+    pass
+
+
+class AtletaOut(Atleta, OutMixin):
+    pass
--- workout_api/workout_api/main.py
+++ workout_api/workout_api/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 
+from workout_api.routers import api_router
+
 app = FastAPI(title="WorkoutAPI")
+app.include_router(api_router)
 
 if __name__ == "main":
     import uvicorn
