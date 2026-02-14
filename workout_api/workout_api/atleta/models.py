148f950f8031dfec579bfa79ea9cd602749d9324
eric-moniz <ericmoniz@outlook.com.br>
2026-02-14 14:07:28 -0300

    Docker compose e configuracao Alembic

--- workout_api/workout_api/atleta/models.py
+++ workout_api/workout_api/atleta/models.py
@@ -1,9 +1,14 @@
 from datetime import datetime
+from typing import TYPE_CHECKING
 
 from sqlalchemy import DateTime, Float, ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from workout_api.contrib.models import BaseModel, CategoriaModel, CentroTreinamentoModel
+from workout_api.contrib.models import BaseModel
+
+if TYPE_CHECKING:
+    from workout_api.categorias.models import CategoriaModel
+    from workout_api.centro_treinamento.models import CentroTreinamentoModel
 
 
 class AtletaModel(BaseModel):
@@ -11,13 +16,13 @@ class AtletaModel(BaseModel):
 
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
     nome: Mapped[str] = mapped_column(String(50), nullable=False)
-    cpf: Mapped[str] = mapped_column(String(11), nullable=False)
+    cpf: Mapped[str] = mapped_column(String(11), unique=True, nullable=False)
     idade: Mapped[int] = mapped_column(Integer, nullable=False)
-    idade: Mapped[float] = mapped_column(Float, nullable=False)
+    peso: Mapped[float] = mapped_column(Float, nullable=False)
     altura: Mapped[float] = mapped_column(Float, nullable=False)
     sexo: Mapped[str] = mapped_column(String(1), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
     categoria: Mapped["CategoriaModel"] = relationship(back_populates="atleta")
-    categoria_id = Mapped[int] = mapped_column(ForeignKey("categorias.pk_id"))
+    categoria_id: Mapped[int] = mapped_column(ForeignKey("categorias.pk_id"))
     centro_treinamento: Mapped["CentroTreinamentoModel"] = relationship(back_populates="atleta")
-    centro_treinamento_id = Mapped[int] = mapped_column(ForeignKey("centros_treinamento.pk_id"))
+    centro_treinamento_id: Mapped[int] = mapped_column(ForeignKey("centros_treinamento.pk_id"))
--- workout_api/workout_api/centro_treinamento/models.py
+++ workout_api/workout_api/centro_treinamento/models.py
@@ -1,14 +1,19 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from workout_api.contrib.models import AtletaModel, BaseModel
+from workout_api.contrib.models import BaseModel
+
+if TYPE_CHECKING:
+    from workout_api.atleta.models import AtletaModel
 
 
 class CentroTreinamentoModel(BaseModel):
     __tablename__ = "centros_treinamento"
 
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    nome: Mapped[str] = mapped_column(String(50), nullable=False)
+    nome: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     endereco: Mapped[str] = mapped_column(String(60), nullable=False)
     proprietario: Mapped[str] = mapped_column(String(30), nullable=False)
     atleta: Mapped["AtletaModel"] = relationship(back_populates="centro_treinamento")
