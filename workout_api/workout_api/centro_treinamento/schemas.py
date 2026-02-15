80ce5d737067897d81b07973cfc021870daafcea
eric-moniz <ericmoniz@outlook.com.br>
2026-02-15 09:55:09 -0300

    Criacao das rotas de Centro de treinamento

--- workout_api/workout_api/centro_treinamento/schemas.py
+++ workout_api/workout_api/centro_treinamento/schemas.py
@@ -1,11 +1,11 @@
 from typing import Annotated
 
-from pydantic import Field
+from pydantic import UUID4, Field
 
 from workout_api.contrib.schemas import BaseSchema
 
 
-class CentroTreinamento(BaseSchema):
+class CentroTreinamentoIn(BaseSchema):
     nome: Annotated[
         str, Field(description="Nome do centro de treinamento", examples=["CT King"], max_length=20)
     ]
@@ -18,3 +18,13 @@ class CentroTreinamento(BaseSchema):
             description="Nome do proprietário do centro de treinamento", examples=["Marcos"], max_length=30
         ),
     ]
+
+
+class CentroTreinamentoAtleta(BaseSchema):
+    nome: Annotated[
+        str, Field(description="Nome do centro de treinamento", examples=["CT King"], max_length=20)
+    ]
+
+
+class CentroTreinamentoOut(CentroTreinamentoIn):
+    id: Annotated[UUID4, Field(description="Identificador de centro de treinamento")]
